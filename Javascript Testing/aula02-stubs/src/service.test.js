1933fe95442dabaaec1a1ff05d00e4e945ed11c6
Italo Gabriel <italo.pereira@pagtel.com.br>
2022-10-20 10:58:54 -0300

    mock and stubs

---
+++ Javascript Testing/aula02-stubs/src/service.test.js
@@ -0,0 +1,18 @@
+const Service = require("./service");
+const {} = require("assert");
+
+const BASE_URL_1 = "https://swapi.dev/api/planets/1/";
+const BASE_URL_2 = "https://swapi.dev/api/planets/2/";
+
+const mocks = {
+  tatooine: require("./mocks/tatooine.json"),
+  alderaan: require("./mocks/alderaan.json"),
+};
+
+(async () => {
+  {
+    const service = new Service();
+    const withoutStub = await service.makeRequest(BASE_URL_2);
+    console.log(JSON.stringify(withoutStub));
+  }
+})();

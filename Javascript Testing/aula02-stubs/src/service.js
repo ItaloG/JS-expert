e42f570e93ab034782082654d70d9ae30d82cf9b
Italo Gabriel <italog03@gmail.com>
2022-10-22 11:40:57 -0300

    feat: add getPlanets method

--- Javascript Testing/aula02-stubs/src/service.js
+++ Javascript Testing/aula02-stubs/src/service.js
@@ -9,6 +9,16 @@ class Service {
       });
     });
   }
+
+  async getPlanets(url) {
+    const result = await this.makeRequest(url);
+
+    return {
+      name: result.name,
+      surfaceWater: result.surface_water,
+      appearedIn: result.films.length,
+    };
+  }
 }
 
 module.exports = Service;

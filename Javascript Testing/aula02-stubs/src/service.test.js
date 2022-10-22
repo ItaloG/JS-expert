98b861b8327b11c7ffb7b6ff9463f1a4396da7a0
Italo Gabriel <italog03@gmail.com>
2022-10-22 11:41:31 -0300

    test: test getPlanets response

--- Javascript Testing/aula02-stubs/src/service.test.js
+++ Javascript Testing/aula02-stubs/src/service.test.js
@@ -1,5 +1,6 @@
 const Service = require("./service");
-const {} = require("assert");
+const sinon = require("sinon");
+const { deepStrictEqual } = require("assert");
 
 const BASE_URL_1 = "https://swapi.dev/api/planets/1/";
 const BASE_URL_2 = "https://swapi.dev/api/planets/2/";
@@ -10,9 +11,33 @@ const mocks = {
 };
 
 (async () => {
+  // {
+  //   const service = new Service();
+  //   const withoutStub = await service.makeRequest(BASE_URL_2);
+  //   console.log(JSON.stringify(withoutStub));
+  // }
+  const service = new Service();
+  const stub = sinon.stub(service, service.makeRequest.name);
+
+  stub.withArgs(BASE_URL_1).resolves(mocks.tatooine);
+  stub.withArgs(BASE_URL_2).resolves(mocks.alderaan);
+
   {
-    const service = new Service();
-    const withoutStub = await service.makeRequest(BASE_URL_2);
-    console.log(JSON.stringify(withoutStub));
+    const expected = {
+      name: "Tatooine",
+      surfaceWater: '1',
+      appearedIn: 5,
+    };
+    const result = await service.getPlanets(BASE_URL_1);
+    deepStrictEqual(result, expected);
+  }
+  {
+    const expected = {
+      name: "Alderaan",
+      surfaceWater: '40',
+      appearedIn: 2,
+    };
+    const result = await service.getPlanets(BASE_URL_2);
+    deepStrictEqual(result, expected);
   }
 })();

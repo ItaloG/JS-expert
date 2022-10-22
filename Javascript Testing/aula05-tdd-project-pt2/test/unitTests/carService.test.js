25306ca25ee6e430951d5b146868bc98635cf4f5
Italo Gabriel <italog03@gmail.com>
2022-10-22 15:34:47 -0300

    feat: add CarService

---
+++ Javascript Testing/aula05-tdd-project-pt2/test/unitTests/carService.test.js
@@ -0,0 +1,7 @@
+const { describe, it } = require('mocha')
+
+describe('CarService Suite Tests', () => {
+    it('given a carCategory it should return an available car ', () => {
+        
+    });
+});

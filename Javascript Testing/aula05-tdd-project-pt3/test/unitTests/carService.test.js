9499393b3c83425846303aaf4d23bd6dbc564b25
Italo Gabriel <italog03@gmail.com>
2022-10-23 11:57:29 -0300

    test: ensure rent method result

--- Javascript Testing/aula05-tdd-project-pt3/test/unitTests/carService.test.js
+++ Javascript Testing/aula05-tdd-project-pt3/test/unitTests/carService.test.js
@@ -1,5 +1,6 @@
 const { describe, it, before, beforeEach, afterEach } = require("mocha");
 const CarService = require("../../src/service/carService");
+const Transaction = require("../../src/entities/transaction");
 
 const { join } = require("path");
 const { expect } = require("chai");
@@ -91,6 +92,38 @@ describe("CarService Suite Tests", () => {
       carCategory,
       numberOfDays
     );
+
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it("given a customer and a car category it should return a transaction receipt", async () => {
+    const car = mocks.validCar;
+    const carCategory = {
+      ...mocks.validCarCategory,
+      price: 37.6,
+      carIds: [car.id],
+    };
+
+    const customer = Object.create(mocks.validCustomer);
+    customer.age = 20;
+
+    const numberOfDays = 5;
+    const dueDate = "10 de novembro de 2020";
+
+    const now = new Date(2020, 10, 5);
+    sandbox.useFakeTimers(now.getTime());
+    sandbox
+      .stub(carService.carRepository, carService.carRepository.find.name)
+      .resolves(car);
+
+    const expectedAmount = carService.currencyFormat.format(206.8);
+    const result = await carService.rent(customer, carCategory, numberOfDays);
+    const expected = new Transaction({
+      customer,
+      car,
+      dueDate,
+      amount: expectedAmount,
+    });
 
     expect(result).to.be.deep.equal(expected);
   });

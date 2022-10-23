a3ff53abd292b9293676fd4e1ba02d57355d8827
Italo Gabriel <italog03@gmail.com>
2022-10-23 10:48:12 -0300

    feat: add getAvailableCar method

--- Javascript Testing/aula05-tdd-project-pt2/src/service/carService.js
+++ Javascript Testing/aula05-tdd-project-pt2/src/service/carService.js
@@ -5,8 +5,21 @@ class CarService {
     this.carRepository = new BaseRepository({ file: cars });
   }
 
-  test() {
-    return this.carRepository.find();
+  getRandomPositionFromArray(list) {
+    const listLength = list.length;
+    return Math.floor(Math.random() * listLength);
+  }
+
+  chooseRandomCar(carCategory) {
+    const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds);
+    const carId = carCategory.carIds[randomCarIndex];
+    return carId;
+  }
+
+  async getAvailableCar(carCategory) {
+    const carId = this.chooseRandomCar(carCategory)
+    const car = await this.carRepository.find(carId)
+    return car;
   }
 }
 

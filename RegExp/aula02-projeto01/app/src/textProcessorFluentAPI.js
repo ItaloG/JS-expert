ac8ea29c382edc50caf5c3897053e7caa0e0c0e8
Italo Gabriel <italog03@gmail.com>
2022-11-05 16:32:49 -0300

    validate regex's

--- RegExp/aula02-projeto01/app/src/textProcessorFluentAPI.js
+++ RegExp/aula02-projeto01/app/src/textProcessorFluentAPI.js
@@ -1,3 +1,4 @@
+const { evaluateRegex } = require("./util");
 // O objetico do Fluent API é executar tarefas
 // como um pipeline, step by step
 // e no fim, chama o build. MUITO similar ao padrão Builder
@@ -24,12 +25,28 @@ class TextProcessorFluentAPI {
     // g -> global
     // m -> multiline
     // i -> insensitive
-    
-    const matchPerson = /(?<=[contratante | contratado]:\s{1})(?!\s)(.*\n.*?)$/gmi
-    const onlyPerson = this.#content.match(matchPerson)
-    // console.log(onlyPerson);
-    this.#content = onlyPerson
 
+    const matchPerson = evaluateRegex(
+      /(?<=[contratante | contratado]:\s{1})(?!\s)(.*\n.*?)$/gim
+    );
+    const onlyPerson = this.#content.match(matchPerson);
+    // console.log(onlyPerson);
+    this.#content = onlyPerson;
+
+    return this;
+  }
+
+  divideTextInColumns() {
+    const splitRegex = evaluateRegex(/,/);
+    this.#content = this.#content.map((line) => line.split(splitRegex));
+    return this;
+  }
+
+  removeEmptyCharacters() {
+    const trimSpaces = evaluateRegex(/^\s+|\s+$|\n/g);
+    this.#content = this.#content.map((line) =>
+      line.map((item) => item.replace(trimSpaces, ""))
+    );
     return this;
   }
 
--- RegExp/aula02-projeto01/app/test/textProcessorFluentAPI.test.js
+++ RegExp/aula02-projeto01/app/test/textProcessorFluentAPI.test.js
@@ -8,6 +8,7 @@ describe("TextProcessorFluentAPI", () => {
     const result = new TextProcessorFluentAPI(mock).build();
     expect(result).to.be.deep.equal(mock);
   });
+
   it("#extractPeopleData", () => {
     const result = new TextProcessorFluentAPI(mock).extractPeopleData().build();
     const expected = [
@@ -19,8 +20,69 @@ describe("TextProcessorFluentAPI", () => {
         "Júlia Menezes, brasileira, solteira, CPF 297.947.800-81, residente e ",
         "domiciliada a Av. dos Estados, 99, bairro Jardins, São Paulo. ",
       ].join("\n"),
-      
     ];
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it("#divideTextInColumns", () => {
+    const content = [
+      [
+        "Xuxa da Silva, brasileira, casada, CPF 235.743.420-12, residente e ",
+        "domiciliada a Rua dos bobos, zero, bairro Alphaville, São Paulo.  ",
+      ].join("\n"),
+    ];
+
+    const result = new TextProcessorFluentAPI(content)
+      .divideTextInColumns()
+      .build();
+
+    const expected = [
+      [
+        "Xuxa da Silva",
+        " brasileira",
+        " casada",
+        " CPF 235.743.420-12",
+        " residente e \ndomiciliada a Rua dos bobos",
+        " zero",
+        " bairro Alphaville",
+        " São Paulo.  ",
+      ],
+    ];
+
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it("#removeEmptyCharacters", () => {
+    const content = [
+      [
+        "Xuxa da Silva",
+        " brasileira",
+        " casada",
+        " CPF 235.743.420-12",
+        " residente e \ndomiciliada a Rua dos bobos",
+        " zero",
+        " bairro Alphaville",
+        " São Paulo.  ",
+      ],
+    ];
+
+    const result = new TextProcessorFluentAPI(content)
+      .removeEmptyCharacters()
+      .build();
+
+    const expected = [
+      [
+        "Xuxa da Silva",
+        "brasileira",
+        "casada",
+        "CPF 235.743.420-12",
+        "residente e domiciliada a Rua dos bobos",
+        "zero",
+        "bairro Alphaville",
+        "São Paulo.",
+      ],
+    ];
+
     expect(result).to.be.deep.equal(expected);
   });
 });

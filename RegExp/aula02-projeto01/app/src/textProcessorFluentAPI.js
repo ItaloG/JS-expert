c01ea996dc84ebd89722dea02bdba113918aaddd
Italo Gabriel <italog03@gmail.com>
2022-11-05 15:11:10 -0300

    regex project

---
+++ RegExp/aula02-projeto01/app/src/textProcessorFluentAPI.js
@@ -0,0 +1,41 @@
+// O objetico do Fluent API é executar tarefas
+// como um pipeline, step by step
+// e no fim, chama o build. MUITO similar ao padrão Builder
+// a diferença que aqui é sobre proecessos, o Builder sobre contrução
+// de objetos
+class TextProcessorFluentAPI {
+  // propriedade privada!
+  #content;
+  constructor(content) {
+    this.#content = content;
+  }
+
+  extractPeopleData() {
+    // ?<= fala que vai extrair os dados que virão depois desse grupo
+    // [contratante | contratado] ou um ou outro, (e tem a flag no fim da expressão para pegar maiusculo e minuculo)
+    // :\s{1} vai procurar o caracter literal do dois pontos seguindo de um espaço
+    // tudo acima fica dentro de um paranteses para falar "vamos pegar daí para frente"
+
+    // (?!s) negative look around, vai ignorar os contratantes do fim do documento (que tem só espaço a frente deles)
+    // .*\n pega qualquer coisa até o primeiro \n
+    // .*? non greety, esse ? faz com que ele pare na primeira recorrência, assim ele evita de ficar em loop
+
+    // $ informa qua a pesquisa acaba no fim da linha
+    // g -> global
+    // m -> multiline
+    // i -> insensitive
+    
+    const matchPerson = /(?<=[contratante | contratado]:\s{1})(?!\s)(.*\n.*?)$/gmi
+    const onlyPerson = this.#content.match(matchPerson)
+    // console.log(onlyPerson);
+    this.#content = onlyPerson
+
+    return this;
+  }
+
+  build() {
+    return this.#content;
+  }
+}
+
+module.exports = TextProcessorFluentAPI;
